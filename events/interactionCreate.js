90409bdb9e9367fc368b3c14f1f808f924a9ebee
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-25 19:52:34 +0530

    lol

---
+++ events/interactionCreate.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        const err_embed = new Discord.MessageEmbed()
+            .setColor('FF0000')
+            .setAuthor(
+                `Error Occured! `,
+                interaction.client.user.displayAvatarURL()
+            )
+            .setTitle('An error occured while executing the command')
+
+        if (!interaction.isCommand()) return
+        if (!interaction.guild)
+            return interaction.reply(
+                `You can use slash commands only in a server!`
+            )
+        const command = interaction.client.commands.get(interaction.commandName)
+
+        if (!command) return
+        try {
+            await command.execute(interaction)
+        } catch (err) {
+            if (err) console.error(err)
+            await interaction.reply({
+                embeds: [err_embed],
+                ephemeral: true,
+            })
+        }
+    },
+}
