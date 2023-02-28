b3afaacc42b09e209710cb59dd25f4fffba33342
Pranshu Patel <pranshu05patel@gmail.com>
2023-02-28 14:40:46 +0530

    lol

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,25 @@
+const Redirect = require("../models/Redirects")
+const Activate = require("../models/Activates")
 module.exports = {
   name: "guildDelete",
   async execute(guild) {
     if (!guild.available) return
+    const activated = await Activate.findOne({guild_id: guild.id})
+    if(!activated) return
+    Activate.deleteOne({ guild_id: guild.id }, (err, settings) => {
+      if(err){
+        console.log(err)
+        return
+      }
+    })
+    const red = await Redirect.findOne({guild_id: guild.id})
+    if(!red) return
+    Redirect.deleteOne({ guild_id: guild.id }, (err, settings) => {
+      if(err){
+        console.log(err)
+        return
+      }
+    })
     console.log(`Server left: ${guild.name}`)
   },
 }
