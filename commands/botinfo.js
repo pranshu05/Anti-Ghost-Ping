1024770aa763b99a0446d7e7ec55fda8de7b6e58
Pranshu Patel <pranshu05patel@gmail.com>
2023-03-04 14:09:03 +0530

    o

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,7 +8,10 @@ fs.readdir(dir, (_err, files) => {
 const Discord = require('discord.js')
 const pkg = require('../package.json')
 const os = require('os')
-const ms = require('ms')
+let days = Math.floor(client.uptime / 86400000 )
+let hours = Math.floor(client.uptime / 3600000 ) % 24 
+let minutes = Math.floor(client.uptime / 60000) % 60
+let seconds = Math.floor(client.uptime / 1000) % 60
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('botinfo')
@@ -22,12 +25,12 @@ module.exports = {
         .setTitle(interaction.client.user.username + ' V: ' + pkg.version + ' ' )
         .addFields(
             { name: '\u200B', value: '\u200B' },
-            {name: '🏠 Guilds', value: `\`\`\`${interaction.client.guilds.cache.size}\`\`\``, inline: true},
-            {name: '🤵 Total Users', value: `\`\`\`${(totalPeople)}\`\`\``, inline: true},
-            {name: ':floppy_disk: System Uptime', value: `\`\`\`${ms(os.uptime(), { long: true })}\`\`\``, inline: true},
-            {name: '🏓 Ping', value: `\`\`\`${(interaction.client.ws.ping).toFixed(0)} ms\`\`\``, inline: true},
-            {name: ':control_knobs: Library', value: `\`\`\`discord.js v${Discord.version}\`\`\``, inline: true},
-            {name: ':computer: Node.js Version', value: `\`\`\`${process.version}\`\`\``, inline: true},
+            {name: '🏠 Guilds', value: `\`\`\`xl\n${interaction.client.guilds.cache.size}\`\`\``, inline: true},
+            {name: '🤵 Total Users', value: `\`\`\`xl\n${(totalPeople)}\`\`\``, inline: true},
+            {name: ':floppy_disk: System Uptime', value:  `\`\`\`xl\n${days}d, ${hours}h, ${minutes}m, ${seconds}s\n\`\`\``, inline: true},
+            {name: '🏓 Ping', value: `\`\`\`xl\n${(interaction.client.ws.ping).toFixed(0)} ms\`\`\``, inline: true},
+            {name: ':control_knobs: Library', value: `\`\`\`xl\ndiscord.js v${Discord.version}\`\`\``, inline: true},
+            {name: ':computer: Node.js Version', value: `\`\`\`xl\n${process.version}\`\`\``, inline: true},
             {name: 'Server', value: '[Click here](https://discord.gg/uJCX5yfuTf)', inline: true},
         )
         interaction.reply({ embeds: [embed] })
